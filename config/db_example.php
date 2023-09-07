ded418aed8f9944d5a3d8d4d38bfbf158f95e298
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-09-07 21:34:44 +0200

    home

---
+++ config/db_example.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+/*    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=asp.roberto.ooo;dbname=disabili_asp',
+    'username' => 'aspdb_user',
+    'password' => '~Qk7xm727',
+    'charset' => 'utf8',*/
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=disabili_asp',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
