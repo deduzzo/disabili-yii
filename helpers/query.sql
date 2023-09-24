81beb5c0dfd9aebbbf2ce8b76cd566a5a97b562d
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-09-24 22:47:35 +0200

    fix mistretta

--- helpers/query.sql
+++ helpers/query.sql
@@ -23,3 +23,13 @@ select i.id, a.cognome_nome from istanza i, anagrafica a where i.id_anagrafica_d
          i.id in (
         select distinct r.id_istanza from recupero r where r.chiuso= false)
 ORDER BY `a`.`cognome_nome` ASC
+
+   UTENTI NON PAGATI IL MESE SCORSO
+   select DISTINCT a.cognome_nome, i.id, i.id_distretto from movimento m, conto c,anagrafica a,istanza i
+where m.id_conto = c.id AND
+        a.id = i.id_anagrafica_disabile AND
+        c.id = m.id_conto
+  AND i.attivo = true AND i.chiuso = false AND i.id not in
+                                               (select i2.id from movimento m2, istanza i2, conto c2
+                                                where m2.id_conto = c2.id AND c2.id_istanza = i2.id AND m2.is_movimento_bancario = true AND m2.data = '2023-08-31')
+ */
