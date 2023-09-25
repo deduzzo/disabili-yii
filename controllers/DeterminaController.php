bc0793a90795ee27bc4eacdf9ed66f7e36418716
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-09-25 13:43:28 +0200

    fix mistretta

--- controllers/DeterminaController.php
+++ controllers/DeterminaController.php
@@ -57,7 +57,7 @@ class DeterminaController extends \yii\web\Controller
             /* @var $istanza Istanza */
             $istanza = Istanza::findOne($istanza['id']);
             $differenza = $istanza->getDifferenzaUltimoImportoArray();
-            if (!$differenza['alert'] && in_array(strval($istanza->id), $allIdPagatiMeseScorso)) {
+            if (!$differenza['alert']) {
                 if ($soloProblematici === "off" || ($soloProblematici == "on" && $differenza['op'] !== "")) {
                     $istVal = [
                         'id' => $istanza->id,
