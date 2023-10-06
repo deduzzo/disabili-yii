16d11573da1b5785e712b705e84d29071bc360f4
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-10-06 12:46:41 +0200

    sistemazione scheda e lista

--- views/determina/simulazione.php
+++ views/determina/simulazione.php
@@ -281,15 +281,10 @@ $formatter = \Yii::$app->formatter;
                 'distretto',
                 [
                     'attribute' => 'isee',
-                    'filter' => Html::activeDropDownList($searchModel, 'isee', ['Maggiore' => IseeType::MAGGIORE_25K, 'Minore' => IseeType::MINORE_25K, "N/D" => IseeType::NO_ISEE], ['class' => 'form-control', 'prompt' => 'Tutti']),
                     'label' => "ISEE",
                     'format' => 'raw',
                     'value' => function ($isee) {
                         return '<span class="badge ' . ($isee === IseeType::MAGGIORE_25K ? IseeType::MAGGIORE_25K_COLOR : ($isee === IseeType::MINORE_25K ? IseeType::MINORE_25K_COLOR : IseeType::NO_ISEE_COLOR)) . '">' . $isee . '</span>';
-                    },
-                    // set column size max 100px and text center
-                    'contentOptions' => function ($model) {
-                        return ['style' => 'width:150px; text-align:center;'];
                     },
                 ],
                 'eta',
