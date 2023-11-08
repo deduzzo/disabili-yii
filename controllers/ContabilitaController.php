57787e67226a2e9045c5192b543b0f8ab1c7b5f6
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-11-08 11:12:20 +0100

    nuovo conto

--- controllers/ContabilitaController.php
+++ controllers/ContabilitaController.php
@@ -65,17 +65,14 @@ class ContabilitaController extends Controller
                 $spesa = (new Query())
                     ->select('SUM(importo)')
                     ->from('movimento')
-                    ->where([
-                        'or',
+                    ->where(
+
                         ['and',
                             ['>=', 'periodo_da', $inizioMese->format('Y-m-d')],
-                            ['<=', 'periodo_a', $fineMese->format('Y-m-d')]
+                            ['<=', 'periodo_da', $fineMese->format('Y-m-d')]
                         ],
-                        ['and',
-                            ['>=', 'data', $inizioMese->format('Y-m-d')],
-                            ['<=', 'data', $fineMese->format('Y-m-d')]
-                        ]
-                    ])
+
+                    )
                     ->andWhere([
                         'is_movimento_bancario' => true,
                         'tornato_indietro' => false
