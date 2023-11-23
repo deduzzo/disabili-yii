0a33f7df064a59fa984deef9bead2b7f5ff989dd
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-11-23 10:40:25 +0100

    nuovo conto

--- helpers/GdriveHelper.php
+++ helpers/GdriveHelper.php
@@ -283,10 +283,7 @@ class GdriveHelper
                             $disabile->codice_fiscale = strtoupper(trim($row[FileGruppiGoogle::CODICE_FISCALE]));
                             $disabile->cognome = strtoupper(trim($row[FileGruppiGoogle::COGNOME]));
                             $disabile->nome = strtoupper(trim($row[FileGruppiGoogle::NOME]));
-                            $dataNascita = Utils::convertDateFromFormat($row[FileGruppiGoogle::DATA_NASCITA_DISABILE]);
-                            if (!$dataNascita)
-                                $dataNascita = strtoupper(trim($row[FileGruppiGoogle::CODICE_FISCALE]));
-                            $disabile->data_nascita = $dataNascita;
+                            $disabile->data_nascita = Utils::getDataNascitaFromCf(strtoupper(trim($row[FileGruppiGoogle::CODICE_FISCALE])));
                             $disabile->indirizzo_residenza = strtoupper(trim($row[FileGruppiGoogle::INDIRIZZO_RESIDENZA_DISABILE]));
                             $disabile->save();
                             if ($disabile->errors)
