3d98f5a5379b0af4965460b7ae6f41cc7d42ac70
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-12-20 19:41:09 +0100

    node test

--- views/contabilita/conti-validi.php
+++ views/contabilita/conti-validi.php
@@ -15,53 +15,34 @@ use yii\helpers\Url;
 /** @var string $result */
 
 
-$this->title = 'Verifica formale codice IBAN';
+$this->title = 'Verifica conti istanze attive';
 $this->params['breadcrumbs'][] = $this->title;
 $formatter = \Yii::$app->formatter;
 ?>
-    <style>
-        /* Centra tutto il contenuto della colonna */
-        .col-md-12 {
-            text-align: center; /* Centra il testo */
-            font-size: larger; /* Aumenta la dimensione del font per tutto il contenuto */
-        }
-
-        /* Stili specifici per l'input dell'IBAN */
-        .iban-input {
-            text-transform: uppercase; /* Trasforma il testo in maiuscolo */
-            font-weight: bold; /* Rende il testo grassetto */
-            width: 500px; /* Imposta la larghezza del campo di input */
-            margin: 0 auto; /* Centra l'input se la larghezza del div è maggiore */
-            display: block; /* Rende l'input un blocco per permettere la centratura con margin auto */
-            font-size: larger; /* Aumenta la dimensione del testo dell'input */
-        }
-
-        /* Stili per la label */
-        .form-label {
-            display: block; /* Fa sì che la label occupi la sua riga */
-            margin-bottom: .5rem; /* Aggiunge un po' di spazio sotto la label */
-            font-size: larger; /* Aumenta la dimensione del font della label */
-        }
-    </style>
     <div class="card">
-    <div class="card-header">
-        <div class="card-toolbar">
-            <!-- list of all new group names -->
-            <?php
-            $istanzeAttive = \app\models\Istanza::findAll(['attivo' => true]);
-            foreach ($istanzeAttive as $istanza) {
-                $contoValido = $istanza->getContoValido();
-                if (!$contoValido && !$istanza->haRicoveriInCorso())
-                    echo "<div class='col-md-12'>ISTANZA <b>" . $istanza->id . "</b> CON CONTO NON VALIDO</div>";
-            }
-            ?>
-
+        <div class="card-header">
+            <div class="card-toolbar">
+                <!-- list of all new group names -->
+                <?php
+                $istanzeAttive = \app\models\Istanza::findAll(['attivo' => true]);
+                $ok = true;
+                foreach ($istanzeAttive as $istanza) {
+                    $contoValido = $istanza->getContoValido();
+                    if (!$contoValido && !$istanza->haRicoveriInCorso()) {
+                        $ok = false;
+                        echo "<div class='col-md-12'><b>#" . $istanza->id . " " . $istanza->getNominativoDisabile() . "</b> CON CONTO NON VALIDO</div>";
+                    }
+                }
+                if ($ok)
+                    echo "✔️Conti validi per tutte le istanze attive";
+                ?>
+            </div>
         </div>
     </div>
-    <?php JSRegister::begin([
-        'key' => 'manage',
-        'position' => \yii\web\View::POS_READY
-    ]); ?>
+<?php JSRegister::begin([
+    'key' => 'manage',
+    'position' => \yii\web\View::POS_READY
+]); ?>
     <script>
 
     </script>
