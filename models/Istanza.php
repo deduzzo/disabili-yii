75cc28b6af161fdf946e28c260aff76c3d058b38
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-03-01 10:54:55 +0100

    nuovo conto

--- models/Istanza.php
+++ models/Istanza.php
@@ -357,6 +357,7 @@ class Istanza extends \yii\db\ActiveRecord
             $restituire = true;
         // add une day to $this->data_decesso
         $totaleGiorniDovuti = Utils::getNumGiorni(!$restituire ? $dataInizioDovuta : Carbon::createFromFormat('Y-m-d', $this->data_decesso)->addDay()->format('Y-m-d'),!$restituire? $this->data_decesso: $dataInizioDovuta);
+        $totaleGiorniDovuti++;
         if ($totaleGiorniDovuti === null)
             return 0;
         $totale =  ($totaleGiorniDovuti['mesi'] * 30 + $totaleGiorniDovuti['giorni']);
