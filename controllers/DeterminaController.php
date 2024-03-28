92dae2eb39141dd9ea6b64ae218ff7ec2ff38a86
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-03-28 12:24:01 +0100

    nuovo conto

--- controllers/DeterminaController.php
+++ controllers/DeterminaController.php
@@ -391,7 +391,7 @@ class DeterminaController extends \yii\web\Controller
             $warning = false;
             foreach ($allIstanze as $istanza) {
                 $istanza = Istanza::findOne($istanza['id']);
-                if ($idDetermina === null)
+                if ($idDetermina === "")
                     die("Errore: determina non trovata");
                 $tempResult = $istanza->verificaContabilitaMese(intval($vars['mese']), intval($vars['anno']), $idDetermina);
                 if ($tempResult['tot'] != 0.0) {
