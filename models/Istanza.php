b245d81f0f5f2d6c8d1a79fd3ff6e284178251d3
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-03-28 12:27:56 +0100

    nuovo conto

--- models/Istanza.php
+++ models/Istanza.php
@@ -538,13 +538,12 @@ class Istanza extends \yii\db\ActiveRecord
         if (!$determina) {
             $inizioMese = Carbon::createFromDate($anno, $mese, 1)->format('Y-m-d');
             $fineMese = Carbon::create($inizioMese)->endOfMonth()->format('Y-m-d');
-            $movimentiIstanzaMese = Movimento::find()->innerJoin('conto c', 'c.id = movimento.id_conto')->where(['c.id_istanza' => $this->id])->andWhere(['>=', 'movimento.data', $inizioMese])->all();
         } else {
             $dataDetermina = $determina->data;
             $inizioMese = $determina->pagamenti_da;
             $fineMese = Carbon::createFromDate($dataDetermina)->endOfMonth()->format('Y-m-d');
-            $movimentiIstanzaMese = Movimento::find()->innerJoin('conto c', 'c.id = movimento.id_conto')->where(['c.id_istanza' => $this->id])->andWhere(['>=', 'movimento.data', $inizioMese])->andWhere(['<=', 'movimento.data', $fineMese])->all();
         }
+        $movimentiIstanzaMese = Movimento::find()->innerJoin('conto c', 'c.id = movimento.id_conto')->where(['c.id_istanza' => $this->id])->andWhere(['>=', 'movimento.data', $inizioMese])->andWhere(['<=', 'movimento.data', $fineMese])->all();
         $logico = 0;
         $reale = 0;
         $ibanLogico = null;
