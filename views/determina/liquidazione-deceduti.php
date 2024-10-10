f18d01e251e7ba38c04891da0122962e5b5a50e1
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-10-10 11:10:51 +0200

    sistemazione scheda e lista

--- views/determina/liquidazione-deceduti.php
+++ views/determina/liquidazione-deceduti.php
@@ -102,13 +102,58 @@ $checkboxColumn = [
 
 ?>
 
+
+
+
+<div class="modal fade text-left" id="finalizza-determina" tabindex="-1" aria-labelledby="label-modifica"
+     style="display: none;"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document"
+         style="min-width: 800px">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title white" id="label-modifica">
+                    Determina liquidazione
+                </h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="feather feather-x">
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                    <i class="bx bx-x d-block d-sm-none"></i>
+                    <span class="d-none d-sm-block">Annulla</span>
+                </button>
+
+                <button type="submit" class="btn btn-warning ms-1">
+                    <i class="bx bx-check d-block d-sm-none"></i>
+                    <span class="d-none d-sm-block">Finalizza determina di liquidazione</span>
+                </button>
+                <?= Html::endForm() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
 <div class="card">
     <div class="card-header">
         <div class="row">
             <div class="col-12">
                 <div class="card-toolbar d-flex align-items-center">
-                    <!--begin::Button-->
-                    <?= Html::submitButton('Liquidazione Deceduti', ['class' => 'btn btn-primary me-3', 'disabled' => true]) ?>
+                    <a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#finalizza-determina"
+                       onclick="check()">Determina Liquidazione</a>
 
                     <!-- Textbox per incollare i valori -->
                     <textarea id="valuesTextbox" rows="2" cols="10" class="form-control me-2"
@@ -118,7 +163,7 @@ $checkboxColumn = [
                     <button id="selezionaCheckboxBtn" class="btn btn-secondary">Seleziona Checkbox</button>
                 </div>
             </div>
-            <div class="col-12">
+            <div class="col-12" style="margin-top:10px">
                 <?= ExportWidget::widget([
                     //'models' => $dataProvider->getModels(),
                     'dataProvider' => $dataProvider,
